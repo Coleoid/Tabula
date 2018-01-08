bf5178566a6c48e6df66e0dd10b785a88712c3b5
Jason Cole <coleoid@yahoo.com>
2018-01-08 13:24:17 -0500

    explorations in dynamic loading of dlls--should be easy, it is their name, after all

--- transpiler/TranspilerTests/WorkflowIntrospectorTests.cs
+++ transpiler/TranspilerTests/WorkflowIntrospectorTests.cs
@@ -1,4 +1,6 @@
 ﻿using NUnit.Framework;
+using System;
+using System.Linq;
 using System.Reflection;
 
 namespace Tabula
@@ -15,5 +17,46 @@ namespace Tabula
 
             Assert.That(method.Name, Is.EqualTo("Snaff_the_Blonker"));
         }
+
+        public Assembly resolveAssembly(object sender, ResolveEventArgs args)
+        {
+            return Assembly.ReflectionOnlyLoadFrom(@"k:\code\acadis_trunk\ScenarioTests\ScenarioContext\bin\Debug\TestopiaAPI.dll");
+        }
+
+        [Test]
+        public void ReflectionOnly_can_reflect_on_scenarios_loaded_dynamically()
+        {
+            AppDomain curDomain = AppDomain.CurrentDomain;
+            curDomain.ReflectionOnlyAssemblyResolve += resolveAssembly;
+
+            //Assembly asm = Assembly.ReflectionOnlyLoadFrom(@"k:\code\acadis_trunk\ScenarioTests\ScenarioContext\bin\Debug\ScenarioContext.dll");
+            var asms = curDomain.GetAssemblies();
+            Assembly asm = asms.SingleOrDefault(a => a.FullName.StartsWith("System.Configuration"));
+
+
+            foreach (var type in asm.ExportedTypes)
+            {
+                Console.WriteLine(type.Name);
+
+                //foreach (var mi in type.GetMethods())
+                //{
+                //    Console.WriteLine(mi.Name);
+                //}
+            }
+
+            //Type t = asm.GetType("Test");
+            //MethodInfo m = t.GetMethod("TestMethod");
+            //ParameterInfo[] p = m.GetParameters();
+
+            //Console.WriteLine("\r\nAttributes for assembly: '{0}'", asm);
+            //ShowAttributeData(CustomAttributeData.GetCustomAttributes(asm));
+            //Console.WriteLine("\r\nAttributes for type: '{0}'", t);
+            //ShowAttributeData(CustomAttributeData.GetCustomAttributes(t));
+            //Console.WriteLine("\r\nAttributes for member: '{0}'", m);
+            //ShowAttributeData(CustomAttributeData.GetCustomAttributes(m));
+            //Console.WriteLine("\r\nAttributes for parameter: '{0}'", p);
+            //ShowAttributeData(CustomAttributeData.GetCustomAttributes(p[0]));
+        }
+
     }
 }
